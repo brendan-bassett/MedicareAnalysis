b2a32e45ed267ccd77b9e4841979ccd64b529ddd
Brendan D Bassett <bbassett3914@gmail.com>
2025-09-02 18:24:17 -0600

    Finished formatting split-segment claims

--- Format_SplitSegmentClaims.sql
+++ Format_SplitSegmentClaims.sql
@@ -7,6 +7,7 @@
 -----------------------------------------------------------------------------------------------------------------------
 */
 
+/*
 
 -- --------------------------------------------------------------------------------------------------------------------
 -- Recover Medicare Analysis Tables from save point at end of Condense_DeSynPUF_2.sql
@@ -21,13 +22,11 @@ DROP TABLE IF EXISTS ma_hcpcs;
 DROP TABLE IF EXISTS ma_ic_icd9_dgns;
 DROP TABLE IF EXISTS ma_ic_icd9_prcdr;
 DROP TABLE IF EXISTS ma_icd;
-DROP TABLE IF EXISTS ma_inpatientclaims;
 DROP TABLE IF EXISTS ma_line_prcsg_ind_cd;
 DROP TABLE IF EXISTS ma_ndc;
 DROP TABLE IF EXISTS ma_oc_hcpcs;
 DROP TABLE IF EXISTS ma_oc_icd9_dgns;
 DROP TABLE IF EXISTS ma_oc_icd9_prcdr;
-DROP TABLE IF EXISTS ma_outpatientclaims;
 DROP TABLE IF EXISTS ma_rxdrugevents;
 DROP TABLE IF EXISTS ma_statecodes;
 
@@ -40,16 +39,23 @@ CREATE TABLE ma_hcpcs AS TABLE ma2_h;
 CREATE TABLE ma_ic_icd9_dgns AS TABLE ma2_ic_icd_d;
 CREATE TABLE ma_ic_icd9_prcdr AS TABLE ma2_ic_icd_p;
 CREATE TABLE ma_icd AS TABLE ma2_i;
-CREATE TABLE ma_inpatientclaims AS TABLE ma2_ic;
 CREATE TABLE ma_line_prcsg_ind_cd AS TABLE ma2_l_p_i_c;
 CREATE TABLE ma_ndc AS TABLE ma2_n;
 CREATE TABLE ma_oc_hcpcs AS TABLE ma2_oc_h;
 CREATE TABLE ma_oc_icd9_dgns AS TABLE ma2_oc_icd_d;
 CREATE TABLE ma_oc_icd9_prcdr AS TABLE ma2_oc_icd_p;
-CREATE TABLE ma_outpatientclaims AS TABLE ma2_oc;
 CREATE TABLE ma_rxdrugevents AS TABLE ma2_rde;
 CREATE TABLE ma_statecodes AS TABLE ma2_state;
 
+--  ---------------------------------
+
+DROP TABLE IF EXISTS ma_inpatientclaims;
+DROP TABLE IF EXISTS ma_outpatientclaims;
+
+CREATE TABLE ma_inpatientclaims AS TABLE ma2_ic;
+CREATE TABLE ma_outpatientclaims AS TABLE ma2_oc;
+
+*/
 
 -- --------------------------------------------------------------------------------------------------------------------
 -- Merge the two-segment claims within Inpatient Claims
@@ -71,9 +77,10 @@ INNER JOIN ma_inpatientclaims_seg2
 ON ma_inpatientclaims.clm_id = ma_inpatientclaims_seg2.clm_id 
 AND ma_inpatientclaims.segment = 1;
 
+
 --      Compare each column side-by-side from segment 1 to segment 2 of each claim
 
-SELECT a.admtng_icd9_dgns_cd, b.admtng_icd9_dgns_cd,
+SELECT a.admtng_icd_dgns_id, b.admtng_icd_dgns_id,
         a.at_physn_npi, b.at_physn_npi,
         a.bs_id, b.bs_id,
         a.clm_admsn_dt, b.clm_admsn_dt,
@@ -119,7 +126,6 @@ SELECT a.clm_id,
         a.op_physn_npi,
         a.ot_physn_npi,
         b.clm_admsn_dt,
-        a.admtng_icd9_dgns_cd,
         a.clm_utlztn_day_cnt,
         b.nch_bene_dschrg_dt,
         a.clm_drg_cd,
@@ -130,7 +136,8 @@ SELECT a.clm_id,
         a.clm_pass_thru_per_diem_amt + b.clm_pass_thru_per_diem_amt,
         b.nch_bene_ip_ddctbl_amt,
         a.nch_bene_pta_coinsrnc_lblty_am + a.nch_bene_pta_coinsrnc_lblty_am,
-        a.nch_bene_blood_ddctbl_lblty_am
+        a.nch_bene_blood_ddctbl_lblty_am,
+        a.admtng_icd_dgns_id
     FROM ma_inpatientclaims_seg2 a
 INNER JOIN ma_inpatientclaims_seg2 b
 ON a.clm_id = b.clm_id
@@ -155,20 +162,65 @@ DROP TABLE IF EXISTS ma_inpatientclaims_seg2_merged;
 ALTER TABLE ma_inpatientclaims DROP COLUMN segment;
 
 
+DROP TABLE IF EXISTS ma_oc_convert;
+CREATE TABLE ma_oc_convert AS TABLE ma_outpatientclaims;
+
 -- --------------------------------------------------------------------------------------------------------------------
--- Format the two-segment claims within Outpatient Claims
+-- Create conversion column for clm_id for Outpatient Claims
 -- --------------------------------------------------------------------------------------------------------------------
+
+--  Both Inpatient and Outpatient claims have multiple segments per claim.
+--  While the Inpatient Claims will be merged, the Outpatient Claims may be merged or split depending on the kind 
+--  of data they contain. We need to create the claim conversion column here so we can identify each 
+
+--  Create a table that lists the claim_ids with duplicates
+
+DROP TABLE IF EXISTS claim_id_conversion;
+CREATE TABLE claim_id_conversion (
+    old_claim_id BIGINT,
+    c BIGINT
+);
+
+INSERT INTO claim_id_conversion
+SELECT clm_id, COUNT(*)
+FROM ma_outpatientclaims 
+GROUP BY clm_id HAVING COUNT(*) > 1;
+
+ALTER TABLE claim_id_conversion DROP COLUMN c;
+
+--  This sequence will increment new claim ids
+
+DROP SEQUENCE IF EXISTS new_clm_id_gen;
+CREATE SEQUENCE new_clm_id_gen
+START 111000000000000
+INCREMENT 1;
+
+ALTER TABLE ma_oc_convert ADD COLUMN new_col_id BIGINT;
+
+UPDATE ma_oc_convert a
+SET new_col_id = clm_id;
+
+UPDATE ma_oc_convert a
+SET new_col_id = nextval('new_clm_id_gen')
+FROM claim_id_conversion b
+WHERE a.clm_id = b.old_claim_id;
+
+DROP TABLE IF EXISTS claim_id_conversion;
+DROP SEQUENCE IF EXISTS new_clm_id_gen;
+
+--      Now each separate claim & segment has an old_id and new_id in the Outpatient Claims table 
+
 
 --      Fill secondary tables with respective segments from each two-segment outpatient claim
 --  -------------------------------------------------------------------------------------
 
 DROP TABLE IF EXISTS ma_outpatientclaims_seg2;
-CREATE TABLE ma_outpatientclaims_seg2 AS TABLE ma_outpatientclaims;
+CREATE TABLE ma_outpatientclaims_seg2 AS TABLE ma_oc_convert;
 
 TRUNCATE TABLE ma_outpatientclaims_seg2;
 
 INSERT INTO ma_outpatientclaims_seg2
-SELECT * FROM ma_outpatientclaims WHERE segment = 2;
+SELECT * FROM ma_oc_convert WHERE segment = 2;
 
 --      RESULT: INSERT 0 11253
 
@@ -179,10 +231,10 @@ CREATE TABLE ma_outpatientclaims_seg1 AS TABLE ma_outpatientclaims_seg2;
 TRUNCATE TABLE ma_outpatientclaims_seg1;
 
 INSERT INTO ma_outpatientclaims_seg1
-SELECT ma_outpatientclaims.* FROM ma_outpatientclaims
+SELECT ma_oc_convert.* FROM ma_oc_convert
 INNER JOIN ma_outpatientclaims_seg2
-ON ma_outpatientclaims.clm_id = ma_outpatientclaims_seg2.clm_id 
-AND ma_outpatientclaims.segment = 1;
+ON ma_oc_convert.clm_id = ma_outpatientclaims_seg2.clm_id 
+AND ma_oc_convert.segment = 1;
 
 --      RESULT: INSERT 0 10975
 
@@ -211,9 +263,9 @@ SELECT * FROM ma_outpatientclaims_nomatch;
 
 --      These have very little relevant information. It seems we can safely drop them from the source table.
 
-DELETE FROM ma_outpatientclaims
+DELETE FROM ma_oc_convert
 USING ma_outpatientclaims_nomatch
-WHERE ma_outpatientclaims.clm_id = ma_outpatientclaims_nomatch.clm_id;
+WHERE ma_oc_convert.clm_id = ma_outpatientclaims_nomatch.clm_id;
 
 DELETE FROM ma_outpatientclaims_seg2
 USING ma_outpatientclaims_nomatch
@@ -221,11 +273,17 @@ WHERE ma_outpatientclaims_seg2.clm_id = ma_outpatientclaims_nomatch.clm_id;
 
 DROP TABLE IF EXISTS ma_outpatientclaims_nomatch;
 
+--      We wont need the old table of converted claim_ids any more. Theyre saved in the ma_outpatientclaims_seg tables
+
+DROP TABLE IF EXISTS ma_oc_convert;
+
 
 -- Compare segment 1 and segment 2 of each claim and determine how to merge or separate their data.
 -- -------------------------------------------------------------------------------------
 
 --      View each column side-by-side from segment 1 to segment 2 of each claim
+
+--      This skips all of the icd and hcpcs codes that will be merged & split accordingly later
 
 SELECT a.clm_id, b.clm_id,
         a.segment, b.segment,
@@ -235,14 +293,14 @@ SELECT a.clm_id, b.clm_id,
         a.at_physn_npi, b.at_physn_npi,
         a.op_physn_npi, b.op_physn_npi,
         a.ot_physn_npi, b.ot_physn_npi,
-        a.admtng_icd9_dgns_cd, b.admtng_icd9_dgns_cd,
         a.bs_id, b.bs_id,
         a.patient_id, b.patient_id,
         a.clm_pmt_amt, b.clm_pmt_amt,
         a.nch_prmry_pyr_clm_pd_amt, b.nch_prmry_pyr_clm_pd_amt,
         a.nch_bene_blood_ddctbl_lblty_am, b.nch_bene_blood_ddctbl_lblty_am,
         a.nch_bene_ptb_ddctbl_amt, b.nch_bene_ptb_ddctbl_amt,
-        a.nch_bene_ptb_coinsrnc_amt, b.nch_bene_ptb_coinsrnc_amt
+        a.nch_bene_ptb_coinsrnc_amt, b.nch_bene_ptb_coinsrnc_amt,
+        a.admtng_icd9_dgns_cd, b.admtng_icd9_dgns_cd
     FROM ma_outpatientclaims_seg1 a
 INNER JOIN ma_outpatientclaims_seg2 b
 ON a.clm_id = b.clm_id
@@ -495,35 +553,122 @@ ON a.clm_id = b.clm_id
 -- Merge the claims where segments 1 and 2 have the same prvdr_num
 -- --------------------------------------------------------------------------------------
 
+DROP FUNCTION IF EXISTS merge_code;
+
+CREATE OR REPLACE FUNCTION merge_code (
+   col_name VARCHAR
+)
+
+RETURNS VARCHAR
+
+LANGUAGE plpgsql
+
+AS $$
+
+DECLARE
+
+    s VARCHAR;
+	
+
+BEGIN
+
+	s :=  format('UPDATE ma_outpatientclaims_seg1 a '
+                    || 'SET %1$I = b.%1$I '
+                    || 'FROM ma_outpatientclaims_seg2 b '
+                    || 'WHERE a.clm_id = b.clm_id '
+                    || 'AND a.prvdr_num = b.prvdr_num '
+                    || 'AND NOT a.%1$I IS NULL;',
+                col_name);
+
+    EXECUTE s;
+
+	RETURN s;
+
+END
+$$;
+
+SELECT merge_code('admtng_icd9_dgns_cd');
+
+SELECT merge_code('hcpcs_cd_1');
+SELECT merge_code('hcpcs_cd_2');
+SELECT merge_code('hcpcs_cd_3');
+SELECT merge_code('hcpcs_cd_4');
+SELECT merge_code('hcpcs_cd_5');
+SELECT merge_code('hcpcs_cd_6');
+SELECT merge_code('hcpcs_cd_7');
+SELECT merge_code('hcpcs_cd_8');
+SELECT merge_code('hcpcs_cd_9');
+SELECT merge_code('hcpcs_cd_10');
+SELECT merge_code('hcpcs_cd_11');
+SELECT merge_code('hcpcs_cd_12');
+SELECT merge_code('hcpcs_cd_13');
+SELECT merge_code('hcpcs_cd_14');
+SELECT merge_code('hcpcs_cd_15');
+SELECT merge_code('hcpcs_cd_16');
+SELECT merge_code('hcpcs_cd_17');
+SELECT merge_code('hcpcs_cd_18');
+SELECT merge_code('hcpcs_cd_19');
+SELECT merge_code('hcpcs_cd_20');
+SELECT merge_code('hcpcs_cd_21');
+SELECT merge_code('hcpcs_cd_22');
+SELECT merge_code('hcpcs_cd_23');
+SELECT merge_code('hcpcs_cd_24');
+SELECT merge_code('hcpcs_cd_25');
+SELECT merge_code('hcpcs_cd_26');
+SELECT merge_code('hcpcs_cd_27');
+SELECT merge_code('hcpcs_cd_28');
+SELECT merge_code('hcpcs_cd_29');
+SELECT merge_code('hcpcs_cd_30');
+SELECT merge_code('hcpcs_cd_31');
+SELECT merge_code('hcpcs_cd_32');
+SELECT merge_code('hcpcs_cd_33');
+SELECT merge_code('hcpcs_cd_34');
+SELECT merge_code('hcpcs_cd_35');
+SELECT merge_code('hcpcs_cd_36');
+SELECT merge_code('hcpcs_cd_37');
+SELECT merge_code('hcpcs_cd_38');
+SELECT merge_code('hcpcs_cd_39');
+SELECT merge_code('hcpcs_cd_40');
+SELECT merge_code('hcpcs_cd_41');
+SELECT merge_code('hcpcs_cd_42');
+SELECT merge_code('hcpcs_cd_43');
+SELECT merge_code('hcpcs_cd_44');
+SELECT merge_code('hcpcs_cd_45');
+
+SELECT merge_code('icd9_dgns_cd_1');
+SELECT merge_code('icd9_dgns_cd_2');
+SELECT merge_code('icd9_dgns_cd_3');
+SELECT merge_code('icd9_dgns_cd_4');
+SELECT merge_code('icd9_dgns_cd_5');
+SELECT merge_code('icd9_dgns_cd_6');
+SELECT merge_code('icd9_dgns_cd_8');
+SELECT merge_code('icd9_dgns_cd_9');
+SELECT merge_code('icd9_dgns_cd_10');
+
+SELECT merge_code('icd9_prcdr_cd_1');
+SELECT merge_code('icd9_prcdr_cd_2');
+SELECT merge_code('icd9_prcdr_cd_3');
+SELECT merge_code('icd9_prcdr_cd_4');
+SELECT merge_code('icd9_prcdr_cd_5');
+SELECT merge_code('icd9_prcdr_cd_6');
+
 --      For every claim that has the same provider number between both segments, 
 --      merge each two-segment claim into a single segment.
 
 DROP TABLE IF EXISTS ma_outpatientclaims_segmerged;
-CREATE TABLE ma_outpatientclaims_segmerged AS TABLE ma_outpatientclaims_seg2;
+CREATE TABLE ma_outpatientclaims_segmerged AS TABLE ma_outpatientclaims_seg1;
 
-TRUNCATE TABLE ma_outpatientclaims_segmerged;
+DELETE FROM ma_outpatientclaims_segmerged a
+USING ma_outpatientclaims_seg2 b
+WHERE a.clm_id = b.clm_id
+    AND a.prvdr_num <> b.prvdr_num;
 
-INSERT INTO ma_outpatientclaims_segmerged
-SELECT a.clm_id,
-        1,
-        a.clm_from_dt,
-        a.clm_thru_dt,
-        a.prvdr_num,    
-        a.at_physn_npi,
-        a.op_physn_npi,
-        a.ot_physn_npi,
-        a.admtng_icd9_dgns_cd,
-        a.bs_id,
-        a.patient_id,
-        a.clm_pmt_amt + b.clm_pmt_amt,
-        a.nch_prmry_pyr_clm_pd_amt + b.nch_prmry_pyr_clm_pd_amt,
-        a.nch_bene_blood_ddctbl_lblty_am,
-        a.nch_bene_ptb_ddctbl_amt, 
-        a.nch_bene_ptb_coinsrnc_amt + b.nch_bene_ptb_coinsrnc_amt
-FROM ma_outpatientclaims_seg1 a
-    INNER JOIN ma_outpatientclaims_seg2 b
-    ON a.clm_id = b.clm_id
-        AND a.prvdr_num = b.prvdr_num;
+UPDATE ma_outpatientclaims_segmerged a
+SET clm_pmt_amt = a.clm_pmt_amt + b.clm_pmt_amt,
+    nch_prmry_pyr_clm_pd_amt = a.nch_prmry_pyr_clm_pd_amt + b.nch_prmry_pyr_clm_pd_amt,
+    nch_bene_ptb_coinsrnc_amt = a.nch_bene_ptb_coinsrnc_amt + b.nch_bene_ptb_coinsrnc_amt
+FROM ma_outpatientclaims_seg2 b
+WHERE a.clm_id = b.clm_id;
 
 
 --      Delete the claims that have been merged from ma_outpatientclaims_seg1 and ma_outpatientclaims_seg2
@@ -540,26 +685,48 @@ WHERE ma_outpatientclaims_seg2.clm_id = ma_outpatientclaims_segmerged.clm_id;
 --      numbers between their corresponding claims.
 
 
---  Replace all the claims in ma_outpatientclaims that have been merged or split
+--  Replace all the old claims in ma_outpatientclaims that will be replaced
 -- --------------------------------------------------------------------------------------
+
+-- Delete from the original table using the old claim_id
 
 DELETE FROM ma_outpatientclaims
 USING ma_outpatientclaims_segmerged
 WHERE ma_outpatientclaims.clm_id = ma_outpatientclaims_segmerged.clm_id;
 
-INSERT INTO ma_outpatientclaims
-SELECT * FROM ma_outpatientclaims_segmerged;
-
 DELETE FROM ma_outpatientclaims
 USING ma_outpatientclaims_seg1
 WHERE ma_outpatientclaims.clm_id = ma_outpatientclaims_seg1.clm_id;
 
+-- Switch over the formatted claims to the new claim_id
+
+UPDATE ma_outpatientclaims_seg1
+SET clm_id = new_col_id;
+
+ALTER TABLE ma_outpatientclaims_seg1
+DROP COLUMN new_col_id;
+
+
+UPDATE ma_outpatientclaims_seg2
+SET clm_id = new_col_id;
+
+ALTER TABLE ma_outpatientclaims_seg2
+DROP COLUMN new_col_id;
+
+
+UPDATE ma_outpatientclaims_seg2
+SET clm_id = new_col_id;
+
+ALTER TABLE ma_outpatientclaims_seg2
+DROP COLUMN new_col_id;
+
+-- Move the formatted claims back into the source table
+
+INSERT INTO ma_outpatientclaims
+SELECT * FROM ma_outpatientclaims_segmerged;
+
 INSERT INTO ma_outpatientclaims
 SELECT * FROM ma_outpatientclaims_seg1;
-
-DELETE FROM ma_outpatientclaims
-USING ma_outpatientclaims_seg2
-WHERE ma_outpatientclaims.clm_id = ma_outpatientclaims_seg2.clm_id;
 
 INSERT INTO ma_outpatientclaims
 SELECT * FROM ma_outpatientclaims_seg2;
