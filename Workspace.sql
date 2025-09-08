5fe69b6a46d5ee664bae7d332294eeca9be2c8c8
Brendan D Bassett <bbassett3914@gmail.com>
2025-09-08 07:54:43 -0600

    minor changes

--- Workspace.sql
+++ Workspace.sql
@@ -1,20 +1,3 @@
 
 
-
---  Add short descriptions
-
-ALTER TABLE ma_hcpcs RENAME COLUMN description TO desc_long;
-ALTER TABLE ma_icd ADD COLUMN desc_short VARCHAR;
-
-UPDATE ma_icd
-SET desc_short = desc_long
-WHERE length(desc_long) <= 29;
-
-UPDATE ma_icd
-SET desc_short = SUBSTRING(desc_long, 1, 29) || '...'
-WHERE length(desc_long) > 29;
-
-UPDATE ma_icd
-SET desc_short = icd9 || ' - unidentified',
-    desc_long = icd9 || ' - unidentified'
-WHERE matched = False;
+VACUUM FULL ANALYZE;
